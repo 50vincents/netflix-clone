860d880808bf2125962ca400bfcb95425573c9ba
50vincents <vincethai035@gmail.com>
2020-10-20 20:45:22 -0700

    banner done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Row from './components/Row';
+import Banner from './components/Banner';
+import Navbar from './components/Navbar';
 import requests from './requests';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Banner />
       <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
       <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
